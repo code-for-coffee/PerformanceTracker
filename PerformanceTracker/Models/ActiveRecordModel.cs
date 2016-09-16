8ae9bc7080aa1b7c171db2fad004303b5c01dab2
code-for-coffee <jtraverjr@gmail.com>
2016-09-15 23:23:47 -0500

    default model interface

---
+++ PerformanceTracker/Models/ActiveRecordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using PerformanceTracker.Models;
+
+namespace PerformanceTracker.Models
+{
+	public interface IDefaultModel<T>
+	{
+		IEnumerable<T> all();
+		T find(string id);
+		T create();
+		Boolean update(KeyValuePair<String, Object> values);
+		Boolean remove(string id);
+	}
+}
+
