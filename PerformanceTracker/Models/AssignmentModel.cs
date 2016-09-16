e6e8020c4ae1ad0b03034b03435985892c541b2b
code-for-coffee <jtraverjr@gmail.com>
2016-09-15 22:56:02 -0500

    git init of project + models

---
+++ PerformanceTracker/Models/AssignmentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PerformanceTracker.Models
+{
+	public class Assignment
+	{
+		public Assignment()
+		{
+		}
+		public Assignment(
+			string name,
+			string desc,
+			double maxScore,
+			double score)
+		{
+			this.name = name;
+			this.description = desc;
+			this.maxPossibleScore = maxScore;
+			this.score = score;
+		}
+
+		public string name { get; set; }
+		public string description { get; set; }
+		public double maxPossibleScore { get; set; }
+		public double score { get; set; }
+
+	}
+}
+
---
+++ PerformanceTracker/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PerformanceTracker
+{
+	public class StudentModel
+	{
+		public StudentModel()
+		{
+		}
+		public string name { get; set; }
+		public string avatarUrl { get; set; }
+		public string group { get; set; }
+		public string
+	}
+}
+
