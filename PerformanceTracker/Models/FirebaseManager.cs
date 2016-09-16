ff5c24df81650b76f731110c51a8e8b6fd48386d
code-for-coffee <jtraverjr@gmail.com>
2016-09-16 17:27:27 -0500

    added Firebase manager

---
+++ PerformanceTracker/Models/FirebaseManager.cs
@@ -0,0 +1,26 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+namespace PerformanceTracker.Models
+{
+    public class FirebaseManager
+    {
+        public FirebaseManager()
+        {
+            config = new FirebaseConfig
+            {
+                AuthSecret = "",
+                BasePath = ""
+            };
+            client = new FirebaseClient(config);
+        }
+        private IFirebaseConfig config;
+        private IFirebaseClient client;
+        public IFirebaseClient db()
+        {
+            return client;
+        }
+    }
+
+}
