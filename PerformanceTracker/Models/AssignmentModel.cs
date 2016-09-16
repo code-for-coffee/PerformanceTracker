47e392310e8bdf964dfbbf8d969e1fb280e516c0
code-for-coffee <jtraverjr@gmail.com>
2016-09-15 22:59:46 -0500

    added enum to assignment model; all default to icebox

--- PerformanceTracker/Models/AssignmentModel.cs
+++ PerformanceTracker/Models/AssignmentModel.cs
@@ -5,6 +5,7 @@ namespace PerformanceTracker.Models
 	{
 		public Assignment()
 		{
+			this.status = statusTypes.icebox.ToString();
 		}
 		public Assignment(
 			string name,
@@ -16,12 +17,21 @@ namespace PerformanceTracker.Models
 			this.description = desc;
 			this.maxPossibleScore = maxScore;
 			this.score = score;
+			this.status = statusTypes.icebox.ToString();
 		}
-
+		enum statusTypes
+		{
+			icebox,
+			thawing,
+			current,
+			testing,
+			passed
+		}
 		public string name { get; set; }
 		public string description { get; set; }
 		public double maxPossibleScore { get; set; }
 		public double score { get; set; }
+		public string status { get; set; }
 
 	}
 }
